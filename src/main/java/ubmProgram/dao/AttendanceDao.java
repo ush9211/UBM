c78eff84d318b67eadf40d818f9376eed1a56e33
hwanayo02 <hwani8129@naver.com>
2024-04-18 15:10:13 +0900

    출석부 완료 (디자인은 아직)
    테스트

---
+++ src/main/java/ubmProgram/dao/AttendanceDao.java
@@ -0,0 +1,247 @@
+package ubmProgram.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+
+import ubmProgram.dto.CDto;
+import ubmProgram.dto.PDto;
+import ubmProgram.dto.SDto;
+
+
+public class AttendanceDao {
+ 
+	PreparedStatement pstmt = null;
+	Statement stmt = null;
+	ResultSet res = null;
+	Connection conn;
+	
+    public AttendanceDao(Connection conn) {
+    	this.conn = conn;
+    }
+    
+    // 하나의 수업을 듣는 모든 학생 수
+    public int CountSelectDB(int cid) {
+    	int rs = 0;
+    	String sql = "select count(case when c_id="+cid+" then c_id end) from s_class";   	
+    	try {
+    		pstmt = conn.prepareStatement(sql);
+      	  	res = pstmt.executeQuery();
+    		    if(res.next()) {
+    		    	rs = res.getInt(1);
+    		 }
+    	}catch(SQLException e) {
+    		e.printStackTrace();
+    	} finally {
+    		try {
+    		   if(res != null) res.close();
+    		   if(stmt != null) stmt.close();
+    		}catch(SQLException e) {e.printStackTrace();}   
+    	}
+    	System.out.println("AllSelectDB 완료");
+    	return rs;
+    }
+    
+  //select student 학생 관련 정보
+    public ArrayList<SDto> s_selectDB(){
+    	
+    	ArrayList<SDto> s_dtos = new ArrayList<>();
+   
+    	String sql = "select * from student order by d_id desc limit ?, ?";
+    	try {
+    	  pstmt = conn.prepareStatement(sql);
+    	  pstmt.setInt(1, 0);
+    	  pstmt.setInt(2, 20);
+    	  	  
+    	  res = pstmt.executeQuery();
+    	
+    	  while(res.next()) {
+    		 int s_id = res.getInt("s_id");
+    		 String s_name = res.getString("s_name");
+    		 String s_pass = res.getString("s_pass");
+    		 String s_tel = res.getString("s_tel");
+    		 String s_email = res.getString("s_email");
+    		 String s_addr = res.getString("s_addr");
+    		 int d_id = res.getInt("d_id");
+    		 Timestamp wdate = res.getTimestamp("wdate");
+    		 
+    		 System.out.println("담기 완료");
+    		 
+    		 SDto sdto = new SDto();
+    		 sdto.setS_id(s_id);
+    		 sdto.setS_name(s_name);
+    		 sdto.setS_pass(s_pass);
+    		 sdto.setS_tel(s_tel);
+    		 sdto.setS_email(s_email);
+    		 sdto.setS_addr(s_addr);
+    		 sdto.setD_id(d_id);
+    		 sdto.setWdate(wdate);
+    		 s_dtos.add(sdto);
+    		 
+    		 System.out.println("입력 준비 완료");
+    	  }
+    	} catch(SQLException e) {
+    		e.printStackTrace();
+    	} finally {
+    		try {
+    		   if(res != null) res.close();
+    		   if(pstmt != null) pstmt.close();
+    		}catch(SQLException e) {e.printStackTrace();}   
+    	}
+    	
+    	System.out.println("정상 작동 중");
+    	
+    	return s_dtos;
+    }
+    
+  //select overload 1) s_class 안에 특정 수업을 듣는 학생을 선별하기 위해 id값을 받아 2) s_class테이블과 join한 student에서 검색하여 정보를 출력 
+    //+ 학과명을 출력하기 위해 department도 join
+    //+ paging 작업중
+      public ArrayList<SDto> s_selectDB(int cid, int limitPage, int listCount){
+      	
+      	ArrayList<SDto> s_dtos = new ArrayList<>();
+     
+      	String sql = "select A.s_id, A.s_name, A.s_email, A.s_tel, A.s_addr, A.d_id, B.c_id, C.d_name "
+      				+"from student as A join s_class as B join department as C on A.s_id = B.s_id and A.d_id = C.d_id "
+      				+"where c_id="+cid+" order by s_id limit ?, ?";
+      	try {
+      	  pstmt = conn.prepareStatement(sql);
+      	  pstmt.setInt(1, limitPage);
+      	  pstmt.setInt(2, listCount);
+      	  	  
+      	  res = pstmt.executeQuery();
+      	
+      	  while(res.next()) {
+      		 int s_id = res.getInt("s_id");
+      		 String s_name = res.getString("s_name");
+      		 String s_tel = res.getString("s_tel");
+      		 String s_email = res.getString("s_email");
+      		 String s_addr = res.getString("s_addr");
+      		 int d_id = res.getInt("d_id");
+      		 String d_name = res.getString("d_name");
+      		 
+      		 System.out.println("담기 완료");
+      		 
+      		 SDto sdto = new SDto();
+      		 sdto.setS_id(s_id);
+      		 sdto.setS_name(s_name);
+      		 sdto.setS_tel(s_tel);
+      		 sdto.setS_email(s_email);
+      		 sdto.setS_addr(s_addr);
+      		 sdto.setD_id(d_id);
+      		 sdto.setD_name(d_name);
+      		 s_dtos.add(sdto);
+      		 
+      		 System.out.println("입력 준비 완료");
+      		 System.out.println(d_name);
+      	  }
+      	} catch(SQLException e) {
+      		e.printStackTrace();
+      	} finally {
+      		try {
+      		   if(res != null) res.close();
+      		   if(pstmt != null) pstmt.close();
+      		}catch(SQLException e) {e.printStackTrace();}   
+      	}
+      	
+      	System.out.println("정상 작동 중");
+      	
+      	return s_dtos;
+      }
+    
+  //select class 수업 관련 모든 정보 출력
+    public ArrayList<CDto> c_selectDB(){
+    	
+    	ArrayList<CDto> c_dtos = new ArrayList<>();
+   
+    	String sql = "select * from class order by d_id desc limit ?, ?";
+    	try {
+    	  pstmt = conn.prepareStatement(sql);
+    	  pstmt.setInt(1, 0);
+    	  pstmt.setInt(2, 20);
+    	  	  
+    	  res = pstmt.executeQuery();
+    	
+    	  while(res.next()) {
+    		 int c_id = res.getInt("c_id");
+    		 String c_name = res.getString("c_name");
+    		 int grade = res.getInt("grade");
+    		 String when = res.getString("when");
+    		 String where = res.getString("where");
+    		 int p_id = res.getInt("p_id");
+    		 int d_id = res.getInt("d_id");
+    		 
+    		 System.out.println("담기 완료");
+    		 
+    		 CDto cdto = new CDto();
+    		 cdto.setC_id(c_id);
+    		 cdto.setC_name(c_name);
+    		 cdto.setGrade(grade);
+    		 cdto.setWhen(when);
+    		 cdto.setWhere(where);
+    		 cdto.setP_id(p_id);
+    		 cdto.setD_id(d_id);
+    		 c_dtos.add(cdto);
+    		 
+    		 System.out.println("입력 준비 완료");
+    	  }
+    	} catch(SQLException e) {
+    		e.printStackTrace();
+    	} finally {
+    		try {
+    		   if(res != null) res.close();
+    		   if(pstmt != null) pstmt.close();
+    		}catch(SQLException e) {e.printStackTrace();}   
+    	}
+    	
+    	System.out.println("정상 작동 중");
+    	
+    	return c_dtos;
+    }
+    
+  //select class 입력받은 수업 id로 한 가지 수업 정보 출력 overload
+    public CDto c_selectDB(int cid){
+       	
+    	CDto cdto = new CDto();
+       	String sql = "select * from class where c_id="+cid;
+
+       	try {
+       	  pstmt = conn.prepareStatement(sql); 
+       	  res = pstmt.executeQuery();
+       	
+       	 while(res.next()) {
+    		 int c_id = res.getInt("c_id");
+    		 String c_name = res.getString("c_name");
+    		 int grade = res.getInt("grade");
+    		 String when = res.getString("when");
+    		 String where = res.getString("where");
+    		 int p_id = res.getInt("p_id");
+    		 int d_id = res.getInt("d_id");
+    		 
+    		 System.out.println("담기 완료");
+    		 
+    		 cdto.setC_id(c_id);
+    		 cdto.setC_name(c_name);
+    		 cdto.setGrade(grade);
+    		 cdto.setWhen(when);
+    		 cdto.setWhere(where);
+    		 cdto.setP_id(p_id);
+    		 cdto.setD_id(d_id);
+    		 
+       	  }
+       	} catch(SQLException e) {
+       		e.printStackTrace();
+       	} finally {
+       		try {
+       		   if(res != null) res.close();
+       		   if(pstmt != null) pstmt.close();
+       		}catch(SQLException e) {e.printStackTrace();}   
+       	}
+       	
+       	return cdto;
+       }
+}
